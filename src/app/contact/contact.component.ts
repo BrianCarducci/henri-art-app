c01d16fdbe4494391fe7b7c6f83c197ff776a9bd
BrianCarducci <briancarducci@gmail.com>
2019-01-08 21:52:59 -0500

    redid front page, handle resume on mobile

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  mobile = false;
 
   constructor() { }
 
   ngOnInit() {
+    console.log(window.screen.width);
+    if (window.screen.width <= 414) { // 768px portrait
+      this.mobile = true;
+    }
+    console.log(this.mobile);
   }
 
 }
